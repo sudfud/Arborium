f92738f7fefe15d50fc778957eca301cb0e3b9d3
Oli <sudsmcfuddle@gmail.com>
2019-02-02 19:16:05 -0500

    Initial commit

---
+++ android/src/com/mygdx/arborium/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.arborium;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.arborium.Arborium;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Arborium(), config);
+	}
+}
